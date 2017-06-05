e3ebf085c7c933e37426fb0f1f24563d4ff15d1e
nick Hill <nick-9979@outlook.com>
2017-06-05 13:36:42 +0100

    Commiting vmss-demo version 2.

--- vmss-demo/App_Start/FilterConfig.cs
+++ vmss-demo/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace vmss_demo
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
--- vmss-demo/Controllers/HomeController.cs
+++ vmss-demo/Controllers/HomeController.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.ApplicationInsights;
 
 namespace vmss_demo.Controllers
 {
     public class HomeController : Controller
     {
+        private TelemetryClient telemetry = new TelemetryClient();
         public ActionResult Index()
         {
+            telemetry.TrackEvent("vmss-demo:homepage");
             return View();
         }
 
