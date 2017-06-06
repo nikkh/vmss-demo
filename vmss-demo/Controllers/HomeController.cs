a5ffdf02acae22f82eb644d0d249dbb0e23895dd
nick Hill <nick-9979@outlook.com>
2017-06-06 20:32:13 +0100

    Version 5 - show environment variables

--- vmss-demo/Controllers/HomeController.cs
+++ vmss-demo/Controllers/HomeController.cs
@@ -4,26 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.ApplicationInsights;
+using System.Collections;
+using vmss_demo.Models;
 
 namespace vmss_demo.Controllers
 {
     public class HomeController : Controller
     {
         private TelemetryClient telemetry = new TelemetryClient();
+
         public ActionResult Index()
         {
             telemetry.TrackEvent("vmss-demo:homepage");
+            ViewBag.MachineName = System.Environment.MachineName;
             return View();
         }
 
         public ActionResult Test()
         {
             telemetry.TrackEvent("vmss-demo:testpage");
-           
-
+            ViewBag.MachineName = System.Environment.MachineName;
             return View();
         }
 
-      
+        public ActionResult Environment()
+        {
+            telemetry.TrackEvent("vmss-demo:environmentpage");
+            ViewBag.MachineName = System.Environment.MachineName;
+            ServerEnvironmentViewModel sevm = new ServerEnvironmentViewModel();
+            return View(sevm);
+        }
+
     }
 }
---
+++ vmss-demo/Models/ServerEnvironmentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vmss_demo.Models
+{
+    public class ServerEnvironmentViewModel
+    {
+        public long WorkingSet { get; }
+        public string ServerName { get; }
+        public Hashtable EnvironmentVariables { get; }
+        public ServerEnvironmentViewModel()
+        {
+            WorkingSet = System.Environment.WorkingSet;
+            ServerName = System.Environment.MachineName;
+            EnvironmentVariables = (Hashtable) System.Environment.GetEnvironmentVariables();
+        }
+    }
+}
